027e13fe9a3a969aeca91e04a411ff9c11f290ca
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-09-20 21:20:08 +0000

    [Update]task3

--- db/migrate/20220919035856_devise_create_users.rb
+++ db/migrate/20220919035856_devise_create_users.rb
@@ -34,7 +34,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       
       ## 名前を保存するカラム
       t.string :name
-
+      
+      #自己紹介文を保存するカラム
+      t.text :introduction
 
       t.timestamps null: false
     end

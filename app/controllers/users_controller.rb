24082bcd131bbb5380ef4eaabec0cda21d4d31b7
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-09-23 07:23:21 +0000

    [Update]task3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,20 +8,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @book = Book.find(params[:id])
   end
-  
+
    def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id), notice: "You have updated user successfully."
    end
-   
+
    def index
      @users = User.all
      #部分テンプレートを使えるようにする（＊current_userはログイン中のユーザーを指している）
      @user = current_user
      @book = Book.new
    end
-   
+
   private
 
    def user_params
